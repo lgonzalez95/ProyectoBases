bd37ddb4e50378b30ba5744188a3a3915d2c17f8
Pánfilo Herrera <luis0270@gmail.com>
2016-05-16 14:25:04 -0600

    Métodos para IMEC clientes, Get y set de entidadCliente

--- Controlador/clsCliente.cs
+++ Controlador/clsCliente.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Modelo;
 
 namespace Controlador
 {
     public class clsCliente
     {
-        
+
+        private string strSentencia;
+
+        public clsCliente()
+        {
+            strSentencia = "";
+        }
+        // Metodo Para Insertar una compra 
+        public Boolean mInsertarCliente(clsConexionSQL cone, clsEntidadCliente pEntidadEntidadCliente)
+        {
+            strSentencia = "insert into cliente(cedula,nombre, apellidos) values('" + pEntidadEntidadCliente.getCedula() +
+                            "', '" + pEntidadEntidadCliente.getNombre() + "' , '" + pEntidadEntidadCliente.getApellidos() + "' )";
+            return cone.mEjecutar(strSentencia, cone);
+        }
+
+        public Boolean mModificarCliente(clsConexionSQL cone, clsEntidadCliente pEntidadEntidadCliente)
+        {
+            strSentencia = "update cliente set nombre = ('" + pEntidadEntidadCliente.getCedula() + "', apellidos = '" + pEntidadEntidadCliente.getApellidos() + "' )";
+            return cone.mEjecutar(strSentencia, cone);
+        }
+
+        // Metodo para seleccionar una compra
+        public SqlDataReader mSeleccionarCliente(clsConexionSQL cone, clsEntidadCliente pEntidadEntidadCliente)
+        {
+            strSentencia = "SELECT * FROM cliente where cedula='" + pEntidadEntidadCliente.getCedula() + "' ;";
+            return cone.mSeleccionar(strSentencia, cone);
+        }
+
+        // Metodo Para eliminar una compra 
+        public Boolean mEliminarClientes(clsConexionSQL cone, clsEntidadCliente pEntidadEntidadCliente)
+        {
+            strSentencia = "DELETE * FROM cliente WHERE cedula='" + pEntidadEntidadCliente.getCedula() + "' ;";
+            return cone.mEjecutar(strSentencia, cone);
+        }
     }
 }
--- Modelo/clsEntidadCliente.cs
+++ Modelo/clsEntidadCliente.cs
@@ -10,10 +10,8 @@ namespace Modelo
     {
         #region Atributos
         private string cedula;
-        private string nombreCompleto;
-        private int tipoCliente;
-        private string telefono;
-        private string email;
+        private string nombre;        
+        private string apellidos;        
         #endregion
 
         #region Propiedades
@@ -23,21 +21,13 @@ namespace Modelo
         {
             this.cedula = cedula;
         }
-        public void setNombreCompleto(string nombreCompleto)
+        public void setNombre(string nombre)
         {
-            this.nombreCompleto = nombreCompleto;
+            this.nombre = nombre;
         }
-        public void setTelefono(string telefono)
+        public void setApellidos(string apellidos)
         {
-            this.telefono = telefono;
-        }
-        public void setEmail(string email)
-        {
-            this.email = email;
-        }
-        public void setTipoCliente(int tipo)
-        {
-            this.tipoCliente = tipo;
+            this.apellidos = apellidos;
         }
 
         //Get
@@ -46,22 +36,15 @@ namespace Modelo
         {
             return this.cedula;
         }
-        public string getNombreCompleto()
+        public string getNombre()
         {
-            return this.nombreCompleto;
+            return this.nombre;
         }
-        public string getTelefono()
+        public string getApellidos()
         {
-            return this.telefono;
+            return this.apellidos;
         }
-        public string getEmail()
-        {
-            return this.email;
-        }
-        public int getTipoCliente()
-        {
-            return this.tipoCliente;
-        }
+
         #endregion
     }
 }
